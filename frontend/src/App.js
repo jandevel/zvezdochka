2253b859d45fa5f4ac1d0d31b8830ceb64aa3d70
Dmitrii Petukhov <dmit.petukhov@gmail.com>
2023-10-29 11:30:58 -0400

    chore(frontend): restructure components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import NavigationMenu from "./components/layout/NavigationMenu";
 import HeaderImage from "./components/layout/HeaderImage";
-import GPXTracksMap from "./components/layout/MapWidget";
+import GPXTracksMap from "./components/map/MapWidget";
 import TestApp from "./components/layout/TestWidget";
 
 function App() {
