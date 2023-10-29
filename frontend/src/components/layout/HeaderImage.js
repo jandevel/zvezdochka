4ffedde48baf8d8a876d81a1c7538ae511d64bd5
Dmitrii Petukhov <dmit.petukhov@gmail.com>
2023-10-29 11:22:52 -0400

    feat(frontend): add placeholder for header image

---
+++ frontend/src/components/layout/HeaderImage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import headerImage from '../../assets/images/t2303 Newfoundland/t2303_150_20230521-1723.jpg';  
+
+import './HeaderImage.css';
+
+function HeaderImage() {
+  return (
+    <div className="header-image">
+      {/* <img src={headerImage} alt="Header Description" /> */}
+    </div>
+  );
+}
+
+export default HeaderImage;
