dacf2bef73b5ff5ace1ecdbd8bf12793174819a9
Dmitrii Petukhov <dmit.petukhov@gmail.com>
2024-07-11 20:19:58 -0400

    feat: add photo gallery to event page

--- backend/routes/getEvent.js
+++ backend/routes/getEvent.js
@@ -14,16 +14,36 @@ const pool = require("../util/db");
  */
 router.get("/api/events/:eventId", async (req, res, next) => {
   try {
-    const eventId = req.params.eventId;    
-    const query = `
+    const eventId = req.params.eventId;
+
+    // Query to retrieve event details
+    const eventQuery = `
       SELECT e.*
       FROM ${process.env.DB_SCHEMA}.events e
       WHERE e.id = $1
     `;
-    const { rows } = await pool.query(query, [eventId]);
-    if (rows.length > 0) {
-      res.json(rows[0]);
-      console.log(rows[0]);
+    const eventData = await pool.query(eventQuery, [eventId]);
+
+    if (eventData.rows.length > 0) {
+      const event = eventData.rows[0];
+
+      // Query to retrieve associated images
+      const imagesQuery = `
+        SELECT file_path, file_name, datetime_original_local
+        FROM ${process.env.DB_SCHEMA}.images
+        WHERE event_id = $1
+        ORDER BY datetime_original_local
+      `;
+      const imagesData = await pool.query(imagesQuery, [eventId]);
+
+      // Combining event data with images
+      const response = {
+        event: event,
+        images: imagesData.rows
+      };
+
+      res.json(response);
+      console.log(response);
     } else {
       res.status(404).send('Event not found');
     }
--- frontend/src/pages/Event/EventDetailPage.jsx
+++ frontend/src/pages/Event/EventDetailPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 const EventDetailPage = () => {
   const [eventDetails, setEventDetails] = useState(null);
@@ -19,11 +21,32 @@ const EventDetailPage = () => {
     fetchEventDetails();
   }, [eventId]);
 
+  const images = eventDetails?.images.map(img => {
+    const baseFileName = img.file_name.split('.').slice(0, -1).join('.'); // Remove extension and keep filename
+    // const extension = img.file_name.split('.').pop(); // Get the extension
+    return {
+      original: `/assets/events/${eventDetails.event.id} ${eventDetails.event.title_en}/images/${img.file_name}`,
+      thumbnail: `/assets/events/${eventDetails.event.id} ${eventDetails.event.title_en}/thumbnails/thumb_${baseFileName}.webp`, // Constructed thumbnail path
+      description: img.datetime_original_local
+    };
+  });
+  console.log(images);
+
+  // // Prepare the images for react-image-gallery
+  // const images = eventDetails?.images.map(img => ({
+  //   original: `/events/${img.event_id} ${eventDetails.event.title_en}/images/${img.file_name}`,
+  //   thumbnail: `/events/${img.event_id} ${eventDetails.event.title_en}/thumbnails/${img.file_name}`,
+  //   description: img.datetime_original_local
+  // }));
+
   return (
     <div>
       {eventDetails ? (
         <div>
-          <h1>{eventDetails.title_en}</h1>
+          <h1>{eventDetails.event.title_en}</h1>
+          {eventDetails.images.length > 0 && (
+            <ImageGallery items={images} />
+          )}
         </div>
       ) : (
         <p>Loading event details...</p>
